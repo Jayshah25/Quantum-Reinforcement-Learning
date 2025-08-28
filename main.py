dc51b687c73d53057639c31a5c8540089f048020
Jay Shah <shahj097@gmail.com>
2025-08-28 18:21:38 +0530

    TestPyPi successfully working

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from qrl.cross_entropy import CrossEntropy
-from qrl.agents import ClassicalNNAgent, RandomQuantumAgent
-from qrl.utils import render_sample_run
+import importlib, traceback
 
-import gymnasium as gym
+modules = [
+    "qrl",
+    "qrl.env",
+    "qrl.env.core",
+    "qrl.env.core.bloch_sphere",
+    "qrl.env.core.expressibility",
+    "qrl.env.core.error_channel",
+    "qrl.env.core.probability",
+    "qrl.env.core.compiler",
+    "qrl.agents",
+    "qrl.agents.base",
+    "qrl.agents.agents",
+]
 
-
-def train_classical_and_quantum_():
-    env = gym.make("CartPole-v1")
-
-    classical_agent = ClassicalNNAgent(input_size=env.observation_space.shape[0],hidden_size=128,num_hidden_layers=2,output_size=env.action_space.n)
-    quantum_agent = RandomQuantumAgent(input_size=env.observation_space.shape[0])
-
-    solver = CrossEntropy(env=env)
-    solver.train_classical_and_quantum(classical_agent=classical_agent,quantum_agent=quantum_agent,plot=True)
-
-render_sample_run(save_path=r"videos/cleaning_robot_episode.mp4")
+for m in modules:
+    print(f">>> importing {m}")
+    try:
+        importlib.import_module(m)
+        print("OK:", m)
+    except Exception as e:
+        print("FAIL:", m, "-", e)
+        traceback.print_exc()
+        break
